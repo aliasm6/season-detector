59ed135ff6cbe92309c9316ad38547a46e439c34
Alias M6 <aliasmontoya719@gmail.com>
2019-04-08 20:35:32 -0700

    Add callback function for error and render error to user

--- src/index.js
+++ src/index.js
@@ -9,20 +9,28 @@ class App extends React.Component {
   constructor(props) {
     //this ensures that React.Component's constructor function is called upon when the App class is created. Otherwise the App class will overwrite the class that it extends upon's constructor. Super is reference to parent's constructor function.
     super(props);
-
-    this.state = { lat: null };
+    //ONLY place where setState function isn't needed.
+    this.state = { lat: null, errorMessage: '' };
 
     window.navigator.geolocation.getCurrentPosition(
       position => {
+        //Used setState function to reasign state!!!!!!
         this.setState({ lat: position.coords.latitude })
       },
-      err => console.log(err)
+      err => {
+        this.setState({ errorMessage: err.message})
+      }
     );
   }
   //React requires that render be defined!!!
   render () {
-
-    return <div> Latitude: {this.state.lat} </div>;
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+      </div>
+    );
   }
 }
 
