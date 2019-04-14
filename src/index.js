654eb48615babe6249415e455a30f9618a6b122f
Alias M6 <aliasmontoya719@gmail.com>
2019-04-14 01:13:20 -0700

    refactor app class by removing constructor method and rreplacing with state declaration

--- src/index.js
+++ src/index.js
@@ -3,17 +3,9 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
-  //special named function particular to JS language not specifc to React
-  //very first function that is called any time an instance of a class is created
 
-  constructor(props) {
-    //this ensures that React.Component's constructor function is called upon when the App class is created. Otherwise the App class will overwrite the class that it extends upon's constructor. Super is reference to parent's constructor function.
-    super(props);
-    //ONLY place where setState function isn't needed.
-    this.state = { lat: null, errorMessage: '' };
-
-
-  }
+  // same as using constructor method
+  state = { lat: null, errorMessage: '' };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
