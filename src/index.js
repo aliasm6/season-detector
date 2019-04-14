02f61fddf923d30a0281574bcf908a023080d678
Alias M6 <aliasmontoya719@gmail.com>
2019-04-14 03:05:25 -0700

    add import for SeasonDisplay component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
+import SeasonDisplay from './SeasonDisplay';
 class App extends React.Component {
 
   // same as using constructor method
@@ -26,7 +25,7 @@ class App extends React.Component {
       return <div>Error: {this.state.ErrorMessage}</div>
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>
+      return <SeasonDisplay lat={this.state.lat} />
     }
 
     return <div> Loading </div>;
